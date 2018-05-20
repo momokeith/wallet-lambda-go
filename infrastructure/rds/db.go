5bfa121ad6f2609c63e26c59d400e2dd0361b146
Mohamed Keita <mohamed.keita@tsi-payment.com>
2018-05-20 21:50:20 +0200

    update

--- infrastructure/rds/db.go
+++ infrastructure/rds/db.go
@@ -8,7 +8,6 @@ import (
 
 func GetDB() *gorm.DB{
 	args := fmt.Sprintf("%s:%s@tcp(%s)/%s?", os.Getenv("DB_USERNAME"),os.Getenv("DB_PASSWORD"),os.Getenv("DB_ENDPOINT_ADDRESS"),os.Getenv("DB_NAME"))
-	println(args)
 	db, err := gorm.Open(os.Getenv("DB_DRIVER"),args)
 	if err != nil {
 		panic("failed to connect database")
